820228bd99bf0827426564f16ea7796dcc4d8ef1
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-08-06 09:33:54 +0530

    Refactor paths_config.py to include data processing paths and enhance common_functions.py with iload_data functions

--- utils/common_functions.py
+++ utils/common_functions.py
@@ -3,8 +3,13 @@ import pandas
 from src.logger import get_logger
 from src.custom_exception import CustomException
 import yaml
+import pandas as pd
+
 
 logger = get_logger(__name__)
+
+
+## Data Ingestion Utility Functions with GCP and YAML support
 
 def read_yaml(file_path):
     try:
@@ -19,4 +24,20 @@ def read_yaml(file_path):
     except Exception as e:
         logger.error(f'Error reading YAML file: {e}')
         raise CustomException(f'Error reading YAML file: {e}')
+
+
+# Data Processing Function to Load Data from CSV
+
+def load_data(file_path):
+    try:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f'CSV file not found at {file_path}')
+        
+        data = pandas.read_csv(file_path)
+        logger.info(f'Successfully loaded data from {file_path}')
+        return data
+    
+    except Exception as e:
+        logger.error(f'Error loading data from CSV file: {e}')
+        raise CustomException(f'Error loading data from CSV file: {e}')
 
