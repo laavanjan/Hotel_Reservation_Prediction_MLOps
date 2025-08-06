e20ce29675cce4bd60044efb7a3a1560bfc630ae
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-08-07 00:35:00 +0530

    Add LightGBM model training implementation and updated requirements.txt

---
+++ src/model_training.py
@@ -0,0 +1,142 @@
+import os
+import pandas as pd
+import joblib
+from sklearn.model_selection import RandomizedSearchCV
+import lightgbm  as lgb
+from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from src.logger import get_logger
+from src.custom_exception import CustomException
+from config.paths_config import *
+from config.model_params import *
+from utils.common_functions import read_yaml,load_data
+from scipy.stats import randint
+import sys
+
+logger = get_logger(__name__)
+
+class ModelTraining:
+
+    def __init__(self,train_path, test_path, model_output_path):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.model_output_path = model_output_path
+
+        self.params_dist = LIGHTGBM_PARAMS
+        self.random_search_params = RANDOM_SEARCH_PARAMS
+
+    
+    def load_and_split_data(self):
+        try:
+            logger.info(f"Loading training  data from: {self.train_path}")
+            train_df = load_data(self.train_path)
+
+            logger.info(f"Loading testing data from: {self.test_path}")
+            test_df = load_data(self.test_path)
+
+            logger.info("Splitting data into features and target variable")
+
+            X_train =train_df.drop(columns=['booking_status'])
+            y_train = train_df['booking_status']
+
+            X_test = test_df.drop(columns=['booking_status'])
+            y_test = test_df['booking_status']
+
+            logger.info('Data split into features and target variable successfully')
+
+            return X_train, y_train, X_test, y_test
+        except Exception as e:
+            logger.error(f"Error in loading and splitting data: {e}")
+            raise CustomException(f'Error in loading and splitting data: {e}', sys.exc_info())
+
+    def train_lgbm(self,X_train, y_train):
+        try:
+            logger.info("Initializing LightGBM model")
+            lgbm_model = lgb.LGBMClassifier(random_state= self.random_search_params['random_state'])
+
+            random_search = RandomizedSearchCV(
+                estimator=lgbm_model,
+                param_distributions=self.params_dist,
+                n_iter=self.random_search_params['n_iter'],
+                cv=self.random_search_params['cv'],
+                n_jobs=self.random_search_params['n_jobs'],
+                verbose=self.random_search_params['verbose'],
+                random_state=self.random_search_params['random_state'],
+                scoring=self.random_search_params['scoring']    
+            )
+
+            logger.info("Starting Hyperparameter tuning")
+
+            random_search.fit(X_train, y_train)
+
+            logger.info("Hyperparameter tuning completed")
+            best_params = random_search.best_params_
+            best_lgbm_model = random_search.best_estimator_
+            
+            logger.info(f'Best parameters are: {best_params}')
+
+            return best_lgbm_model
+        except Exception as e:
+            logger.error(f"Error in training LightGBM model: {e}")
+            raise CustomException(f'Error in training LightGBM model: {e}', sys.exc_info())
+        
+    def evaluate_model(self, model, X_test, y_test):
+        try:
+            logger.info('Evaluating model performance on test data')
+
+            y_pred = model.predict(X_test)
+
+            accuracy = accuracy_score(y_test, y_pred)
+            precision = precision_score(y_test, y_pred)
+            recall = recall_score(y_test, y_pred)
+            f1 = f1_score(y_test, y_pred)
+
+            logger.info(f'Accuracy Score: {accuracy}')
+            logger.info(f'Precision Score: {precision}')
+            logger.info(f'Recall Score: {recall}')
+            logger.info(f'F1 Score: {f1}')
+
+            return{
+                'accuracy': accuracy,
+                'precision': precision,
+                'recall': recall,
+                'f1_score': f1
+            }
+        except Exception as e:
+            logger.error(f"Error in evaluating model: {e}")
+            raise CustomException(f'Error in evaluating model: {e}', sys.exc_info())
+        
+    def save_model(self, model):
+        try:
+            os.makedirs(os.path.dirname(self.model_output_path), exist_ok=True)
+
+            logger.info(f"Saving model to: {self.model_output_path}")
+            joblib.dump(model, self.model_output_path)
+            logger.info("Model saved successfully")
+        except Exception as e:
+            logger.error(f"Error in saving model: {e}")
+            raise CustomException(f'Error in saving model: {e}', sys.exc_info())
+        
+    
+
+    def run(self):
+        try:
+            logger.info("Starting model training process")
+
+            X_train, y_train, X_test, y_test = self.load_and_split_data()
+
+            model = self.train_lgbm(X_train, y_train)
+
+            evaluation_metrics = self.evaluate_model(model, X_test, y_test)
+
+            self.save_model(model)
+
+            logger.info("Model training process completed successfully")
+
+            return evaluation_metrics   
+        except Exception as e:
+            logger.error(f"Error in model training process: {e}")
+            raise CustomException(f'Error in model training process: {e}', sys.exc_info())
+        
+if __name__ == "__main__":
+    trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH, PROCESSED_TEST_DATA_PATH, MODEL_OUTPUT_PATH)
+    trainer.run()
